b2ce67f1103594d5ff7212321f6e3741b64f8498
blsoko <exanymejose1@hotmail.com>
2020-02-05 21:03:03 +0000

    dwj

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf ("Size of a char: %c\n", sizeof(charType), " bit(s)");
+	printf ("Size of an int: %d\n", sizeof(doubleType), " bit(s)");
+	printf ("Size of a long int: %ld\n", sizeof(charType), " bit(s)");
+	printf ("Size of a long long int: %lld\n", sizeof(charType), " bit(s)");
+	printf ("Size of a float: %f\n", sizeof(floatType), " bit(s)");
+	return (0);
+}
