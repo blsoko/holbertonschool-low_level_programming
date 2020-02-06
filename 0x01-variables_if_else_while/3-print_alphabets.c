8a1a3db4fe0ba7ce7097fde0fe967f20508e4d79
blsoko <exanymejose1@hotmail.com>
2020-02-06 20:07:12 +0000

    sj

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 'a';
+	int A = 'A';
+	int countnum = 0;
+	int countnum1 = 0;
+
+	while (countnum != 26)
+	{
+		putchar(a++);
+		countnum++;
+
+	}
+	while (countnum1 != 26)
+	{
+		putchar(A++);
+		countnum1++;
+		if (countnum1 == 26)
+		{
+			putchar('$');
+			putchar('\n');
+			countnum1 = 0;
+			break;
+		}
+	}
+
+
+	return (0);
+}
