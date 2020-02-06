72308a39738d510dbea872838f05b8b841e41eff
blsoko <exanymejose1@hotmail.com>
2020-02-06 04:09:59 +0000

    see

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str1[60];
+
+	strcpy(str1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	puts(str1);
+	return (1);
+}
