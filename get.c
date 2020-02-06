afaee13cb38d64244b9f30cc8924fd66964f4e6f
blsoko <exanymejose1@hotmail.com>
2020-02-06 03:19:00 +0000

    kalinka

--- get.c
+++ get.c
@@ -2,9 +2,6 @@
 #include <string.h>
 int main(void)
 {
-	char str1[52];
-	strcpy(str1, "\"Programming is like building a multilingual puzzle");
-	puts(str1);
-	return(0);
-#!/bin/bash
-gcc get.c
+	printf ("saw");
+	return (0);
+}
