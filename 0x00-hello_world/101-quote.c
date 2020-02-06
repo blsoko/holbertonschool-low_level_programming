5fbabe76ab94f95a570c7d2dcc898009278d066e
blsoko <exanymejose1@hotmail.com>
2020-02-06 05:05:08 +0000

    last try

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
  */
 int main(void)
 {
-	system("echo and that piece of art is useful'\"'  - Dora Korpar, 2015-10-19");
-	return (1);
+system("echo and that piece of art is useful'\"' - Dora Korpar, 2015-10-19\n");
+return (1);
 }
