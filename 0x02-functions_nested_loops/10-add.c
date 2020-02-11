084ec0ff6e675edaecb2bb2ca910a8225de6a0ef
blsoko <exanymejose1@hotmail.com>
2020-02-11 18:09:26 +0000

    dun

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,12 +3,12 @@
 /**
 * add - writes the character c to stdout
 * @c: The character to print
-* @n: The character to print 
+* @n: The character to print
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
 
 int add(int n, int c)
 {
-	return (n+c);
+	return (n + c);
 }
