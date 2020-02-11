fa1e72dbe7597913b50f0352603b97f7bd973b06
blsoko <exanymejose1@hotmail.com>
2020-02-11 15:19:29 +0000

    ss

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
-
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
+ * print_alphabet - check the code for Holberton School students.
+ * print_alphabet se
+ * Return: Always 0.
+ */
 
 void print_alphabet(void)
 {
-	 int i;
-	 for (i = 97; i < 123; i++)
-	 {
-	 _putchar(i);
-	 }
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
 	_putchar('\n');
 }
