d01f8b6011ca0deb18ae373809b5e7ad48275998
blsoko <exanymejose1@hotmail.com>
2020-02-11 18:08:10 +0000

    sdj

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* add - writes the character c to stdout
+* @c: The character to print
+* @n: The character to print 
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+
+int add(int n, int c)
+{
+	return (n+c);
+}
