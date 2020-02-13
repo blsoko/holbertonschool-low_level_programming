fa37a9150a7c8fd1caac973e869d7167c5008840
blsoko <exanymejose1@hotmail.com>
2020-02-13 03:43:00 +0000

    sdh

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+* main - writes the character c to stdout
+*
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+
+int main(void)
+{
+	long int f1, f2, result;
+	int count;
+
+	f1 = 1;
+	f2 = 2;
+	for (count = 0; count < 51; count++)
+	{
+		result = f1 + f2;
+		f1 = f2;
+		f2 = result;
+		if (count != 50)
+		{
+			printf("%ld, ", result);
+		}
+		else
+		{
+			printf("%ld", result);
+		}
+	}
+	printf("\n");
+}
