53b6e5b2064b1dfabaed72389829e1661de60078
blsoko <exanymejose1@hotmail.com>
2020-02-14 23:30:21 +0000

    dw

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,45 +1,39 @@
 #include "holberton.h"
-#include <math.h>
-
+#include <stdio.h>
 /**
-* print_number - Entry point
-* @n: value 
-*
-* Return: Always 0 (Success)
-*/
-void print_number(int n)
+  * print_number(int n) - Entry point
+  * @n: :( 
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
 {
-	int digit, o_count, result, i_count, m_count;
-	if (n == 0)
+	int a, n, potencia, last, store, boolean;
+	n = -98027;
+	if (n < 0)
 	{
-		digit=1;
-	} 
-	else if (n > 0)
-		for (m_count = 1; m_count <= 100000; m_count * 10)
+		n *= -1;
+		printf("-\n");
+		printf("%d\n", n);
+	}
+	a = 0;
+	potencia = 10;
+	while (1 > a)
+	{
+		n /= potencia;
+		potencia *= 10;
+		last %= n;
+		if (n >= 0 && n < 10)
 		{
-			if(n >= (m_count) && n < (m_count * 10))
-			{
-				digit = m_count + 1;
-			}
+			printf("%d\n", n);
+			a = 1;
+		}
+		else
+		{
+			printf("%d", last);
+		}
+	}	
 
-		}
-	else
-	{
-		_putchar('-');
-		n = n * -1;
-		for (m_count = 1; m_count <= 100000; m_count * 10)
-		{
-			if(n >= (m_count) && n < (10 ** (m_count * 10)))
-			{
-				digit = m_count+1;
-			}
-		}
-	}
-	for (i_count = digit; i_count > 0; i_count--)
-	{
-	for (o_count = i_count; 
-		result = (n % (10 ** digit));
-		result = (n / (10 ** digit));
-		_putchar(result + '0');
-	}
+
+
 }
