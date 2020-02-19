ae78abca5c67ed99d2b8bc5cc341eee8052eb104
blsoko <exanymejose1@hotmail.com>
2020-02-19 03:06:59 +0000

    wd

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * rev_string - check the code for Holberton School students.
+ * @s: pointer
+ *
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int index, count, iterador;
+	char new[100000];
+	for (index = 0; s[index] != '\0' ; index++)
+	{
+	}
+	iterador = index;
+	for (count = 0; count < index; count++)
+	{
+		iterador--;
+		new[iterador] = s[count];
+	}
+
+	for (count = 0; count <= index; count++)
+	{
+		s[count] = new[count];
+	}
+	
+}
