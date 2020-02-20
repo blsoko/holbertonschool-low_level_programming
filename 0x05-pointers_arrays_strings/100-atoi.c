e41a3da2533a4e14b130faa6c4a620730831448b
blsoko <exanymejose1@hotmail.com>
2020-02-20 00:27:43 +0000

    dww

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,28 +16,17 @@ int _atoi(char *s)
 	ans = 0;
 	while (i >= 0)
 	{
-		cncel = 0;
 		if (s[i] == '-')
 		{
 			signo *= -1;
 		}
 
-		if (s[i] < 10 && s[i] >= 0)
+		else if (s[i] < 10 && s[i] >= 0)
 		{
-			for (aux = i; aux <= i; i++)
-			{
-				if (s[i] < 10 && s[i] >= 0)
-				{
-					cncel = 1;
-					ans = ans * 10 + s[i];
-				}
-				else
-				{
-					i = -1;
-				}
-			}
+			cncel = 1;
+			ans = ans * 10 + s[i];
 		}
-		if (cncel == 1)
+		else if (cncel == 1 && !(s[i] < 10 && s[i] >= 0))
 		{
 			i = -1;
 		}
