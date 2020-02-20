6b66b95d897b025481367bd66f7714194bfa1b44
blsoko <exanymejose1@hotmail.com>
2020-02-20 21:33:55 +0000

    dw

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+* reverse_array - return char
+*
+* @a: value
+* @n: value
+*
+*
+* Return: Always 0 (Success)
+*/
+
+void reverse_array(int *a, int n)
+{
+	int cont, iterador;
+	int narray[100000];
+
+	iterador = n;
+	for (cont = 0; cont < n; cont++)
+	{
+		iterador--;
+		narray[iterador] = a[cont];
+	}
+	for (cont = 0; cont <= n; cont++)
+	{
+		a[cont] = narray[cont];
+	}
+}
