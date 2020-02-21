ea300d78bfc46ee9c2ea94fd68ca726864681db6
blsoko <exanymejose1@hotmail.com>
2020-02-21 22:18:04 +0000

    wddw

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,12 +7,21 @@
 *
 * Return: Always 0 (Success)
 */
-char *leet(char d*)
+char *leet(char *d)
 {
-	char narray[];
-	for (count = 0; d[count] ; count++)
-	{	
-			
+	int cont, i;
+	char a[6] = {'a','e','o','t','l'};
+	char A[6] = {'A','E','O','T','L'};
+	char n[6] = {'4','3','0','7','1'};
+	for (cont = 0; d[cont]; cont++)
+	{
+		for (i = 0; a[i]; i++)
+		{
+			if (a[i] == d[cont] || A[i] == d[cont])
+			{
+				d[cont] = n[i];
+			}
+		}
 	}
 	return (d);
 }
