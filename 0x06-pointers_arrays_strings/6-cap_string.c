18f11d7a47e3bb31e2bce1ddb16ecaae952cae2e
blsoko <exanymejose1@hotmail.com>
2020-02-22 04:22:51 +0000

    wd

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *d)
 	char signs[] = {' ', '\n', '\t', ',', ';', '.', '!',
 	 '?', '"', '(', ')', '{', '}'};
 
-	for (cont = 0; d[cont] ; cont++)
+	for (cont = 1; d[cont] ; cont++)
 	{
 		for (i = 0; signs[i]; i++)
 		{
@@ -23,6 +23,11 @@ char *cap_string(char *d)
 				d[cont + 1] = d[cont + 1] - 32;
 			}
 		}
+	}
+	cont = 0;
+	if (d[cont] >= 97 && d[cont] <= 122)
+	{
+		d[cont] = d[cont] - 32;
 	}
 	return (d);
 }
