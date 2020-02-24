19755635097644a84bc19fd9cf5f2b90240e2489
blsoko <exanymejose1@hotmail.com>
2020-02-24 23:09:00 +0000

    dw

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -24,15 +24,15 @@ char *_strstr(char *haystack, char *needle)
 			}
 
 		}
+	if (haystack[i + j] == needle[j])
+	{
 		if (needle[j] == '\0')
 		{
 			return (&haystack[i]);
 		}
-		else if (needle[j+1] == '\0')
-		{
-			return (&haystack[i]);
-		}
 	}
+	}
+
 	if (haystack[i] == '\0')
 	{
 		return (haystack);
