7dc502e6a1e5763038ea612890568bc2b07d7597
blsoko <exanymejose1@hotmail.com>
2020-02-25 03:33:35 +0000

    wd

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * set_string - to another
+ *
+ * @s: array pointer
+ * @to: what type of value
+ *
+ * Return: a multidemnsional array
+ */
+
+void set_string(char **s, char *to)
+{
+	*to = **s;
+}
