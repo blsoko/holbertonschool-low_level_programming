5be83c74910b9d3f12cc121a92649c35e47cc150
blsoko <exanymejose1@hotmail.com>
2020-02-26 19:14:12 +0000

    dw

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,10 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	_putchar(s[i]);
-	i++;
-	if (s[i] != '\0')
+	if (*s != '\0')
 	{
-		_puts_recursion(s + i);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
