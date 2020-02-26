b1a97101065cfd8a07ef539482c990bba081e409
blsoko <exanymejose1@hotmail.com>
2020-02-26 23:24:53 +0000

    dw

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - function
+ * @s: value
+ *
+ * Return: value of function;
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+	if ()
+	{
+		_pow_recursion(x * y, y);
+	}
+}
