67298583902bfdb279162284d2c97e6f59737f3c
blsoko <exanymejose1@hotmail.com>
2020-02-27 04:52:38 +0000

    dw

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,10 +9,11 @@
 
 int _strlen_recursion(char *s)
 {
-
-	if (*s != '\0')
+	if (*s != 0)
 	{
-		return (_strlen_recursion(s + 1) );
+
+		s++;
+		return (1 + _strlen_recursion(s));
 	}
-	
+	return (0);
 }
