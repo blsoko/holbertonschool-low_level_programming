e3e8e3dcd7a6bba9b9e59e1f1a36754d58df53c8
blsoko <exanymejose1@hotmail.com>
2020-02-29 18:56:57 +0000

    multiplier 2 numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,21 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * main - check
+ *
+ * @argc: value
+ * @argv: value
+ *
+ * Return: check;
+ */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i, mult = 1;
+
 	if (argc == 3)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			mult *= atoi(argv[i]);	
+			mult *= atoi(argv[i]);
 		}
-		printf ("%d\n", mult);
-		return(0);
+		printf("%d\n", mult);
+		return (0);
 	}
 	else
 	{
-		printf ("Error\n");
-		return(1);
+		printf("Error\n");
+		return (1);
 	}
 }
