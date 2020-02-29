5d0519dccab9f293d45495b9e89265c61e17f2fe
blsoko <exanymejose1@hotmail.com>
2020-02-29 18:44:08 +0000

    print name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,8 @@
  *
  * Return: check;
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return (argc);
+	return (0);
 }
