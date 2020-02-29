3d25bad1a619ec21321e7234b9aad3b9ba27c46e
blsoko <exanymejose1@hotmail.com>
2020-02-29 18:54:04 +0000

    arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-int main(int argc, char* argv[])
+/**
+ * main - check
+ *
+ * @argc: value
+ * @argv: value
+ *
+ * Return: check;
+ */
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf ("%s\n", argv[i]);
+		printf("%s\n", argv[i]);
 	}
-	return(0);
+	return (0);
 }
