4dd77a96c28687c07646516ac9077ef1c6322e14
blsoko <exanymejose1@hotmail.com>
2020-02-29 18:30:54 +0000

    print name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+/**
+ * main - check
+ *
+ * @argc: value
+ * @argv: value
+ *
+ * Return: check;
+ */
 int main(int argc, char *argv[])
 {
-	printf ("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (argc);
 }
