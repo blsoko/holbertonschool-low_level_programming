8015b13a69a1fc8812cfc6d2392fa745eb66c328
blsoko <exanymejose1@hotmail.com>
2020-02-29 20:14:16 +0000

    machine coin

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,13 +10,25 @@
  * Return: check;
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 	int cents = 0;
 	int num[5] = {25, 10, 5, 2, 1};
 	int number = atoi(argv[1]);
 	int i = 0;
 
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+	if (number < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 	while (number != 0)
 	{
 		for (i = 0; i < 5; i++)
@@ -30,5 +42,7 @@ int main(int argc __attribute__((unused)), char *argv[])
 		}
 	}
 	printf("%d\n", cents);
+	return (0);
+	}
 	return (0);
 }
