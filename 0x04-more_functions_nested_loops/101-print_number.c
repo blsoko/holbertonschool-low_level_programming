c2a71aa899e755c1f94ac972affc8ac57d1d08bd
blsoko <exanymejose1@hotmail.com>
2020-03-02 15:38:11 +0000

    print number with putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,39 +1,23 @@
 #include "holberton.h"
-#include <stdio.h>
+
 /**
-  * print_number(int n) - Entry point
-  * @n: :( 
-  *
-  * Return: Always 0 (Success)
-  */
-int main(void)
+* print_number - return char
+*
+* @n: value
+* Return: Always 0 (Success)
+*/
+
+void print_number(int n)
 {
-	int a, n, potencia, last, store, boolean;
-	n = -98027;
 	if (n < 0)
 	{
-		n *= -1;
-		printf("-\n");
-		printf("%d\n", n);
+		n = -n;
+		_putchar('-');
 	}
-	a = 0;
-	potencia = 10;
-	while (1 > a)
+	while (n / 10)
 	{
-		n /= potencia;
-		potencia *= 10;
-		last %= n;
-		if (n >= 0 && n < 10)
-		{
-			printf("%d\n", n);
-			a = 1;
-		}
-		else
-		{
-			printf("%d", last);
-		}
-	}	
-
-
-
+		print_number(n / 10);
+		break;
+	}
+	_putchar((n % 10) + '0');
 }
