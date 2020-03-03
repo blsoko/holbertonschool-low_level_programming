73a93258dd0b389091c92517918463529647fbf8
blsoko <exanymejose1@hotmail.com>
2020-03-03 20:47:17 +0000

    check

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,8 +16,17 @@ char *str_concat(char *s1, char *s2)
 	int ite;
 	int kte;
 	int size = 0;
+	int size_2 =0;
 	char *ptr; /*string concatenate*/
 
+	if (s1 == NULL)
+	{
+		return ("");
+	}
+	if (s2 == NULL)
+	{
+		return ("");
+	}
 	for (ite = 0; s1[ite]; ite++)
 	{
 		size++;
@@ -25,10 +34,9 @@ char *str_concat(char *s1, char *s2)
 
 	for (ite = 0; s2[ite]; ite++)
 	{
-		size++;
+		size_2++;
 	}
-	size++;
-	ptr = malloc(size * sizeof(char));
+	ptr = malloc((size_2 + size + 1) * sizeof(char));
 	if (ptr == NULL)
 	{
 		return (NULL);
@@ -37,7 +45,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		*(ptr + ite) = *(s1 + ite);
 	}
-
 	for (kte = 0; s2[kte]; kte++)
 	{
 		*(ptr + ite) = *(s2 + kte);
