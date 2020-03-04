ba0501b1b6734611833e956250ac842ba69aec42
blsoko <exanymejose1@hotmail.com>
2020-03-04 04:01:54 +0000

    string

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -14,6 +14,7 @@
 char liss(char *number)
 {
 	int i = 0;
+	
 	while (number[i] != '\0')
 	{
 		i++;
@@ -21,38 +22,54 @@ char liss(char *number)
 	return (i);
 }
 
-
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, law = 0; /*iterator*/
+	int i, j = 0, k = 0; /*iterator*/
 	char *vector;
-	int size = 0;
+	int size = 0; /*total size*/
 
 	if (ac == 0 || av[0] == NULL)
 	{
 		return (NULL);
 	}
+	/*size of the array*/
 	for (i = 0; i < ac; i++)
 	{
 		size += liss(av[i]);
-		size++;
+		size++; /*line jump*/
 	}
 	size++;
 
 	vector = malloc(size * sizeof(char));
 	if (vector == NULL)
 	{
-		return (0);
+		return (NULL);
 	}
-	for (j = 0; j < ac; j++)
+	/*creating the string*/
+	for (i = 0; i < ac; i++)
 	{
-		law = law + k;
-		for (k = 0; av[k]; k++)
+		while (av[i][k] != '\0')
 		{
-			vector[law + k] = av[j][k];
+			if (av[i][k] == ' ')
+			{
+				vector[j] = ' ';
+				vector[j + 1] = '\n';
+				j += 2;
+				break;
+			}
+			vector[j] = av[i][k];
+			j++;
+			if (vector == NULL)
+			{
+				return (NULL);
+			}
+			else
+			{
+				k++;
+			}
 		}
-		vector[law + k] = '\n';
+		k = 0;
 	}
-	vector[law] = '\0';
+	vector[j] = '\0';
 	return (vector);
 }
