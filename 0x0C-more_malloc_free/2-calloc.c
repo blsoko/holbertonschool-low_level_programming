b270214829dd0ccd3f92518d0fc6d4a9e5251d14
blsoko <exanymejose1@hotmail.com>
2020-03-05 20:33:02 +0000

    array bits

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,24 +1,35 @@
 #include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * _calloc - Entry point
  *
- * @nmemb: Value
- * @size: Value
+ * @nmemb: value
+ * @size: value
  *
  * Return: Always 0 (Success)
  */
 
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *vector;
+	unsigned int j;
+	char *vector;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
+	{
 		return (NULL);
+	}
 	vector = malloc(nmemb * size);
 	if (vector == NULL)
+	{
+		free(vector);
 		return (NULL);
+	}
+	for (j = 0; j < (size * nmemb); j++)
+	{
+		vector[j] = '0';
+	}
 	return (vector);
 }
