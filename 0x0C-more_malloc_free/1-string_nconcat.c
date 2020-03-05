6b6ce84515907014f90edbfc3f72baf0f0c723d4
blsoko <exanymejose1@hotmail.com>
2020-03-05 20:14:02 +0000

    concatenate ultimate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,13 +17,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *vector;
 	int i, k; /*iterator*/
-	unsigned int x;
+	unsigned int x, j;
 
 	if (s1 == NULL)
 		s1 = "";
 	else if (s2 == NULL)
 		s2 = "";
 	for (i = 0; s1 && s1[i]; i++)
+	{
+	}
+	for (j = 0; s2 && s2[j]; j++)
 	{
 	}
 	vector = malloc((i + n + 1) * sizeof(char));
@@ -36,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		vector[k] = s1[k];
 	}
-	for (x = 0; x < n && s2[x] ; x++)
+	for (x = 0; x < n && x < j ; x++)
 	{
 		vector[k] = s2[x];
 		k++;
