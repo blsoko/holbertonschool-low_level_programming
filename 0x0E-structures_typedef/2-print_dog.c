4e48a809a9e4f077cb81f06bc72f8e482ae54814
blsoko <exanymejose1@hotmail.com>
2020-03-09 23:17:32 +0000

    structure index in values

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - Entry point
+ *
+ * @d: parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+			d->name = "(nil)";
+		}
+		else if (d->owner == NULL)
+		{
+			d->owner = "(nil)";
+		}
+		printf("Name: %s\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+}
--- 0x0E-structures_typedef/holberton.h
+++ 0x0E-structures_typedef/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON
 #define HOLBERTON
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 int _putchar(char c);
 #endif /* HOLBERTON */
