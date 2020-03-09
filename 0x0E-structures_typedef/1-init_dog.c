53e7903215d57bfcc3a7b84d8fc7357937f77bbe
blsoko <exanymejose1@hotmail.com>
2020-03-09 22:16:40 +0000

    structures

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - Entry point
+ *
+ * @d: parameter
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner dog
+ *
+ * Return: Always 0 (Success)
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/holberton.h
@@ -0,0 +1,5 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+void init_dog(struct dog *d, char *name, float age, char *owner);
+int _putchar(char c);
+#endif /* HOLBERTON */
