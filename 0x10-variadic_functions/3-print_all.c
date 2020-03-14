9f822e067275c30445cd05e920fe1e129e176af5
blsoko <exanymejose1@hotmail.com>
2020-03-14 22:07:30 +0000

    structures

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,4 @@
-#include <stdarg.h>
+
 #include <stdio.h>
 #include "variadic_functions.h"
 
@@ -10,52 +10,52 @@
  *
  * Return: Always 0 (Success)
  */
-int integer(va_list a)
+void integer(va_list a)
 {
-
+	printf("%d, ", va_arg(a, int));
 }
-char caracter(va_list a)
+void caracter(va_list a)
 {
-
+	printf("%c, ", va_arg(a, int));
 }
-char string(va_list a)
+void string(va_list a)
 {
-
-}
-float intfloat(va_list a)
-{
-
-}
-void (*anything(char *s))(va_list a)
-{
-	struct funct newstr[] = {
-		{"i", integer},
-		{"c", caracter},
-		{"s", string},
-		{"f", intfloat},
-		{NULL, NULL}
-	};
-	int i = 0;
-
-	while (newstr[i].op != 0)
+	if(a == NULL)
 	{
-		if (newstr[i].op[0] == s[0])
-		{
-			return (ops[i].f);
-            return;
-		}
-		i++;
+		printf("(nil)");
+		return;
 	}
+	printf("%s, ", va_arg(a, char*));
+}
+void intfloat(va_list a)
+{
+	printf("%f, ", va_arg(a, double));
 }
 
  void print_all(const char * const format, ...)
  {
      va_list record;
-     unsigned int i = 0;
-
-     while(format[i])
-     {
-         anything(format)(record);
-         i++;
-     }
+     int i = 0, j = 0;
+	
+	 op_t newstr[] = {
+		{"i", integer},
+		{"c", caracter},
+		{"s", string},
+		{"f", intfloat},
+	};
+	va_start(record, format);
+	while (format[j] != 0)
+	{
+		while (i < 4)
+		{
+			if (newstr[i].op[0] == format[j])
+			{
+				(newstr[i].f)(record);
+			}
+			i++;
+		}
+		j++;
+		i = 0;
+	}
+	va_end(record);
  }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdarg.h>
 int _putchar(char c);
+typedef struct old
+{
+	    char *op;
+	    void (*f)(va_list);
+} op_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
