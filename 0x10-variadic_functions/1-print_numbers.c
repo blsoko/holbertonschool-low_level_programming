88885dce08ee17c83dae5eedae431ee9a95403c3
blsoko <exanymejose1@hotmail.com>
2020-03-15 19:47:47 +0000

    check missing

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,33 +1,34 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "variadic_functions.h"
 
 /**
  * print_numbers - Entry point
  *
- * @separator: n
- * @n: value
+ * @separator: value
+ * @n: valueddd
  *
- * Return: Always 0 (Success)
- */
+ * Return: Alwaydds 0 (Success)
+*/
 
- void print_numbers(const char *separator, const unsigned int n, ...)
- {
-    unsigned int i;
-    va_list record;
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list record;
 
-    if(n == 0)
-    {
-        return;
-    }
-    va_start(record, n);
-    if (separator == NULL)
-        separator = "";
-    for (i = 0; i < n - 1; i++)
-    {
-            printf("%d%s", va_arg(record, const unsigned int), separator);
-    }
-    printf("%d", va_arg(record, const unsigned int));
-    printf("\n");
-    va_end(record);
- }
+	if (n == 0)
+	{
+		return;
+	}
+	va_start(record, n);
+	if (separator == NULL)
+		separator = "";
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%i%s", va_arg(record, int), separator);
+	}
+	printf("%i", va_arg(record, int));
+	printf("\n");
+	va_end(record);
+}
