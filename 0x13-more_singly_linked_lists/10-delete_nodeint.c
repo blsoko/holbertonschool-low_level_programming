70dcb8e7bab84ac9d2c76ba5056f1ea5b4c2bab4
blsoko <exanymejose1@hotmail.com>
2020-03-24 17:53:01 +0000

    delete node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - remove the head at the end linked list
+ *
+ * @head: Header
+ * @index: index
+ *
+ * Return: The position of the last add node, or null if fails
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *vector, *reference;
+	unsigned int iterator = 0;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	vector = *head;
+	if (index == 0)
+	{
+		*head = vector->next;
+		free(vector);
+		return (1);
+	}
+	for (iterator = 0; vector != NULL && iterator < index - 1; iterator++)
+	{
+		vector = vector->next;
+	}
+
+	if (vector == NULL || vector->next == NULL)
+	{
+		return (-1);
+	}
+	reference = vector->next->next;
+	free(vector->next);
+	vector->next = reference;
+	return (1);
+}
