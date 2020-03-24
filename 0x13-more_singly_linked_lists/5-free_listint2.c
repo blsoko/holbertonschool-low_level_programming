12e2d0272a6e18d3e3331ba8b08f9768f8e31d9d
blsoko <exanymejose1@hotmail.com>
2020-03-24 15:02:37 +0000

    free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free list
+ *
+ * @head: Header
+ *
+ * Return: The newest node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *vector;
+
+	while (*head != NULL)
+	{
+		vector = *head;
+		*head = (*(head))->next;
+		free(vector);
+	}
+}
