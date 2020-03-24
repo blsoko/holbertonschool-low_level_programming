c5b5629c628bd1e3f64610b4d9681fc8064f7838
blsoko <exanymejose1@hotmail.com>
2020-03-24 15:55:57 +0000

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,15 +1,48 @@
 #include "lists.h"
 
 /**
- * sum_listint - remove the head at the end linked list
+ * insert_nodeint_at_index - remove the head at the end linked list
  *
  * @head: Header
+ * @idx: index
+ * @n: number to add
  *
- * Return: The newest node
+ * Return: The position of the last add node, or null if fails
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-        
-        
+	listint_t *vector, *reference;
+	unsigned int iterator = 0;
+
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+	vector = malloc(sizeof(listint_t));
+	if (vector == NULL)
+	{
+		return (NULL);
+	}
+	vector->n = n;
+	vector->next = NULL;
+	if (idx == 0)
+	{
+		vector->next = *head;
+		*head = vector;
+		return (vector);
+	}
+	reference = *head;
+	while (reference != NULL)
+	{
+		if (iterator == (idx - 1))
+		{
+			vector->next = reference->next;
+			reference->next = vector;
+			return (vector);
+		}
+	reference = reference->next;
+	iterator++;
+	}
+	return (NULL);
 }
