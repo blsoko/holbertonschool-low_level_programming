729194ecf28d5f2f65dfbc9a9b1bfe32949bf427
blsoko <exanymejose1@hotmail.com>
2020-03-25 18:47:08 +0000

    print safe

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - free list
+ *
+ * @head: Header
+ *
+ * Return: The newest node
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t iterator = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (head != NULL)
+	{
+		if (head - (head->next) > 0)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			iterator++;
+		}
+		else
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			printf("-> [%p] %d\n", (void *)head, head->next->n);
+			break;
+		}
+		head = head->next;
+	}
+	iterator++;
+	return (iterator);
+}
