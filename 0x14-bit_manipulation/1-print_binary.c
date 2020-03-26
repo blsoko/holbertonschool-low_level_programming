382b3a7ce3ced25127572deb0a3d25bb7b46cc31
blsoko <exanymejose1@hotmail.com>
2020-03-26 21:41:26 +0000

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,7 +10,7 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int ref = 0;
-	long int i = 0;
+	int i = 0;
 	int flag = 0;
 
 	if (n == 0)
@@ -18,7 +18,7 @@ void print_binary(unsigned long int n)
 		_putchar('0');
 		return;
 	}
-	for (i = 31; i >= 0; i--)
+	for (i = 63; i >= 0; i--)
 	{
 		ref = n >> i;
 		if (ref & 1 || flag == 1)
