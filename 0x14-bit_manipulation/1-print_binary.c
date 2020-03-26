54516e15822c522d1efc05d8489b7522a4aeb642
blsoko <exanymejose1@hotmail.com>
2020-03-26 21:32:36 +0000

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * print_binary - free list
+ *
+ * @n: number to bytes
+ *
+ * Return: The newest node
+ */
+void print_binary(unsigned long int n)
+{
+	long int i = 0, ref = 0;
+	int flag = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	for (i = 31; i >= 0; i--)
+	{
+		ref = n >> i;
+		if (ref & 1 || flag == 1)
+		{
+			flag = 1;
+			if (ref & 1)
+			{
+				_putchar('1');
+			}
+			else
+			{
+				_putchar('0');
+			}
+		}
+	}
+}
