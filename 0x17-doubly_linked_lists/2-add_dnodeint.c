26c323884089c3a2f726fb4d7f64c92b6ffdaf22
blsoko <exanymejose1@hotmail.com>
2020-05-04 02:17:55 +0000

    check

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,8 +18,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	vector->n = n;
-	vector->next = *head;
+	vector->next = (*head);
 	vector->prev = NULL;
-	*head = vector;
+	if ((*head) != NULL)
+		(*head)->prev = vector;
+	(*head) = vector;
 	return (vector);
 }
