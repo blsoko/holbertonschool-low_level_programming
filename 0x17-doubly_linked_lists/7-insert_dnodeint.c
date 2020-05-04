c00697f53a34073dbba6587a73c00030b282cf73
blsoko <exanymejose1@hotmail.com>
2020-05-04 02:46:03 +0000

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,17 +12,13 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *vector, *reference;
-	unsigned int iterator = 0;
+	unsigned int i;
 
 	if (h == NULL)
-	{
 		return (NULL);
-	}
 	vector = malloc(sizeof(dlistint_t));
 	if (vector == NULL)
-	{
 		return (NULL);
-	}
 	vector->n = n;
 	vector->next = NULL;
 	if (idx == 0)
@@ -31,16 +27,24 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = vector;
 		return (vector);
 	}
-	reference = *h;
-	for (iterator = 0; reference != NULL ; iterator++)
+	vector = *h;
+	i = 0;
+	while (i < idx - 1 && vector != NULL)
 	{
-		if (iterator == (idx - 1))
-		{
-			vector->next = reference->next;
-			reference->next = vector;
-			return (vector);
-		}
-		reference = reference->next;
+		vector = vector->next;
+		i++;
+	}
+	if (vector != NULL)
+	{
+		reference = malloc(sizeof(dlistint_t));
+		if (reference == NULL)
+			return (NULL);
+		reference->n = n;
+		reference->next = vector->next;
+		reference->prev = vector;
+		if (reference->next != NULL)
+			vector->next->prev = reference;
+		vector->next = reference;
 	}
 	return (NULL);
 }
