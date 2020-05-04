25630a1ee2a4a5eee8b38d735fb5b8e9ad0e274a
blsoko <exanymejose1@hotmail.com>
2020-05-04 01:44:16 +0000

    stage

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+ * deleteNode - remove the head at the end linked list
+ *
+ * @head: Header
+ * @del: index
+ *
+ * Return: 1 if succed or -1 if its wrong
+ */
+int deleteNode(dlistint_t **head, dlistint_t *del)
+{
+	if (*head == NULL || del == NULL)
+		return (-1);
+	if (*head == del)
+		*head = del->next;
+	if (del->next != NULL)
+		del->next->prev = del->prev;
+	if (del->prev != NULL)
+		del->prev->next = del->next;
+	free(del);
+	return (1);
+}
+/**
  * delete_dnodeint_at_index - remove the head at the end linked list
  *
  * @head: Header
@@ -8,34 +29,24 @@
  *
  * Return: The position of the last add node, or null if fails
  */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *vector, *reference;
-	unsigned int iterator = 0;
+	unsigned int i;
+	dlistint_t *vector;
+	int value = 0;
 
 	if (*head == NULL)
-	{
 		return (-1);
-	}
 	vector = *head;
-	if (index == 0)
-	{
-		*head = vector->next;
-		free(vector);
-		return (1);
-	}
-	for (iterator = 0; vector != NULL && iterator < index - 1; iterator++)
+	/*go to the pos*/
+	for (i = 1; vector != NULL && i < index; i++)
 	{
 		vector = vector->next;
 	}
-
-	if (vector == NULL || vector->next == NULL)
-	{
+	if (vector == NULL)
 		return (-1);
-	}
-	reference = vector->next->next;
-	free(vector->next);
-	vector->next = reference;
-	return (1);
+	value = deleteNode(head, vector);
+	if (value == 1)
+		return (1);
+	return (-1);
 }
