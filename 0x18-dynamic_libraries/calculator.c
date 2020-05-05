9f3f4f0fca2f63e2ab7594aa860cedc454aa7c95
blsoko <exanymejose1@hotmail.com>
2020-05-05 01:18:17 +0000

    advanced task

---
+++ 0x18-dynamic_libraries/calculator.c
@@ -0,0 +1,64 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * add - mathematic operation
+ *
+ * @a: first value
+ * @b: second value
+ *
+ * Return: add
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - mathematic operation
+ *
+ * @a: first value
+ * @b: second value
+ *
+ * Return: sub
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - mathematic operation
+ *
+ * @a: first value
+ * @b: second value
+ *
+ * Return: mul
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - mathematic operation
+ *
+ * @a: first value
+ * @b: second value
+ *
+ * Return: div
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - mathematic operation
+ *
+ * @a: first value
+ * @b: second value
+ *
+ * Return: mod
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
