b98d9a608ba0b00234244e415332945a81afda8e
blsoko <exanymejose1@hotmail.com>
2020-05-30 01:05:46 +0000

    creation of hash

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create a hash table
+ *
+ * @size: size of the hash
+ *
+ * Return: pointer to the newly created hash table or NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int count;
+	hash_table_t *vector;
+
+	vector = malloc(sizeof(hash_table_t));
+	if (vector == NULL)
+		return (NULL);
+	vector->size = size;
+	vector->array = malloc(size * sizeof(hash_node_t *));
+	if (vector->array == NULL)
+		return (NULL);
+	for (count = 0; count < size; )
+		vector->array[count] = NULL;
+	return (vector);
+}
