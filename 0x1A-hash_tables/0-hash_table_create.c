084b10817c4fb94b3b667ae0aecff04ec6dd71fe
blsoko <exanymejose1@hotmail.com>
2020-05-30 01:20:51 +0000

    checker issue

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,13 +13,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int count;
 	hash_table_t *vector;
 
-	vector = malloc(sizeof(hash_table_t));
+	vector = malloc(sizeof(hash_table_t *));
 	if (vector == NULL)
 		return (NULL);
 	vector->size = size;
-	vector->array = malloc(size * sizeof(hash_node_t));
+	vector->array = malloc(size * sizeof(hash_node_t *));
 	if (vector->array == NULL)
+	{
+		free(vector->array);
 		return (NULL);
+	}
 	for (count = 0; count < size; )
 		vector->array[count] = NULL;
 	return (vector);
