3a949476e67dcdb1f0c568404162488a3cdce242
blsoko <exanymejose1@hotmail.com>
2020-11-06 11:43:35 -0500

    5-island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            start = 4
+            if grid[i][j] == 1 and i != len(grid) and grid[i + 1][j] == 1:
+                start -= 1
+            if grid[i][j] == 1 and i != 0 and grid[i - 1][j]:
+                start -= 1
+            if grid[i][j] == 1 and j != len(grid[i - 1]) and grid[i][j + 1]:
+                start -= 1
+            if grid[i][j] == 1 and j != 0 and grid[i][j - 1]:
+                start -= 1
+            if grid[i][j] == 1:
+                perimeter += start
+    return perimeter
