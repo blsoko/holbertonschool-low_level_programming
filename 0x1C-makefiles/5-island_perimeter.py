abfa980b88980c8ad1999e5da4492adb14f5dd0e
blsoko <exanymejose1@hotmail.com>
2020-11-06 12:51:28 -0500

    fix out of range

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,11 +8,11 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             start = 4
-            if grid[i][j] == 1 and i != len(grid) and grid[i + 1][j] == 1:
+            if grid[i][j] == 1 and i != len(grid) - 1 and grid[i + 1][j] == 1:
                 start -= 1
             if grid[i][j] == 1 and i != 0 and grid[i - 1][j]:
                 start -= 1
-            if grid[i][j] == 1 and j != len(grid[i - 1]) and grid[i][j + 1]:
+            if grid[i][j] == 1 and j != len(grid[i]) - 1 and grid[i][j + 1]:
                 start -= 1
             if grid[i][j] == 1 and j != 0 and grid[i][j - 1]:
                 start -= 1
